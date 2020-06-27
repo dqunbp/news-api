2fb85594fb443a255fdbd29b4e85c9895ad862a1
AleksandrHexlet <46378431+AleksandrHexlet@users.noreply.github.com>
2020-06-27 18:19:26 +0300

    added OPTIONS in Allow-Methods

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://news-today.site');
   }
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
   next();
 });
