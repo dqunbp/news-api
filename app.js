5aea68502be2319cb6e20cbd4ceccc776a8bbe44
AleksandrHexlet <46378431+AleksandrHexlet@users.noreply.github.com>
2020-06-27 17:38:07 +0300

    upgrade CORS

--- app.js
+++ app.js
@@ -31,17 +31,30 @@ const allowedCors = [
   'http://localhost:8080',
 ];
 
+// app.use((req, res, next) => {
+//   const { origin } = req.headers; // Записываем в переменную origin соответствующий заголовок
+
+//   if (allowedCors.includes(origin)) {
+//     // Проверяем, что значение origin есть среди разрешённых доменов
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//   }
+
+//   next();
+// });
+
 app.use((req, res, next) => {
-  const { origin } = req.headers; // Записываем в переменную origin соответствующий заголовок
-
-  if (allowedCors.includes(origin)) {
-    // Проверяем, что значение origin есть среди разрешённых доменов
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.headers.origin && allowedCors.includes(req.headers.origin)) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+  } else {
+    res.header('Access-Control-Allow-Origin', 'https://news-today.site');
   }
-
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
   next();
 });
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
