5c42b1e1ccc18ca3c9e6912217af54c57d34fce2
dqunbp <dqunbp@gmail.com>
2020-06-28 15:09:40 +0300

    add emain and name to login response

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,12 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const user = require('../models/users');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const user = require("../models/users");
 const {
   BadAuthenticationError,
   AccessDenied,
   BadEmail,
-} = require('../constructorError/error');
-const { JWT_SECRET } = require('../config');
+} = require("../constructorError/error");
+const { JWT_SECRET } = require("../config");
 
 const { NODE_ENV } = process.env;
 
@@ -19,11 +19,8 @@ module.exports.getUser = (req, res, next) => {
     .catch(next);
 };
 
-
 module.exports.createUser = (req, res, next) => {
-  const {
-    email, password, name,
-  } = req.body;
+  const { email, password, name } = req.body;
   bcrypt.hash(password, 10).then((hash) => {
     user
       .create({
@@ -36,7 +33,7 @@ module.exports.createUser = (req, res, next) => {
       // .then((newUser) => res.status(200).send(newUser))
       // .catch(next);
       .catch(() => {
-        const err = new BadEmail('Не удалось создать пользователя.');
+        const err = new BadEmail("Не удалось создать пользователя.");
         return next(err);
       });
   });
@@ -44,24 +41,25 @@ module.exports.createUser = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-  user.findOne({ email })
-    .select('+password')
+  user
+    .findOne({ email })
+    .select("+password")
     .then((newUser) => {
       if (!newUser) {
-        throw new BadAuthenticationError('Неправильные почта или пароль');
+        throw new BadAuthenticationError("Неправильные почта или пароль");
       }
-      return bcrypt.compare(password, newUser.password)
-        .then((matched) => {
-          if (!matched) {
-            throw new AccessDenied('Неправильные почта или пароль');
-          }
-          const token = jwt.sign(
-            { _id: newUser._id },
-            NODE_ENV === 'production' ? JWT_SECRET : 'JWT_SECRET',
-            { expiresIn: '7d' },
-          );
-          return res.send({ token });
-        });
+      return bcrypt.compare(password, newUser.password).then((matched) => {
+        if (!matched) {
+          throw new AccessDenied("Неправильные почта или пароль");
+        }
+        const token = jwt.sign(
+          { _id: newUser._id },
+          NODE_ENV === "production" ? JWT_SECRET : "JWT_SECRET",
+          { expiresIn: "7d" }
+        );
+        const { email, name } = newUser;
+        return res.send({ email, name, token });
+      });
     })
     .catch(next);
 };
